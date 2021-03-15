ecb9600301bc26042e6603b1c588cdb5d1505db2
Nutro <nutroutro@gmail.com>
2021-03-15 20:13:34 +0300

    Typical-arrays-problems commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (typeof array != 'object') {return 0} else{ 
+        var min = array[0];
+    if (typeof min == 'undefined'){return 0} else{
+        for(i = 0, l = array.length; i < l; i++){
+            if(array[i] < min) min = array[i];
+        }      
+    return min;
+}}
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (typeof array != 'object') {return 0} else{
+        var max = array[0];
+    if (typeof max == 'undefined'){return 0} else{
+        for(i = 0, l = array.length; i < l; i++){
+            if(array[i] > max) max = array[i];
+        }
+return max;
+}}}
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (typeof array != 'object' || array.length == 0) {return 0} else{
+    var result = array.reduce(function(pre, curr){
+        if (typeof curr == 'undefined'){return 0} else{
+        return pre + curr;}
+    }, 0);
+        
+    return result / array.length;
+}}
